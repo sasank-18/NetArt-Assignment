1b3c1a2dc0f3bcd10205f9fc86ed78c33b9f7c90
sasank Prajapati <sasank45prajapati@gmail.com>
2024-02-28 00:25:41 +0530

    seond Commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,20 +5,20 @@ import world from "../asserts/globe-solid.svg";
 
 const Footer = () => {
   return (
-    <div className="bg-red-600 py-9 flex px-[10px] justify-center  gap-y-6 flex-wrap text-white">
-    <div className="icon flex justify-center mr-20 items-center ">
-    <img className="w-8 mr-3" src={call} />
-      <h3>Toll free 1800 200 1234</h3>
-    </div>
-      <div className="icon flex justify-center mr-20 items-center">
-      <img className="w-9 mr-3" src={facebook} />
-      <h3>www.facebook.com/cripumps</h3>
+    
+    <div className="bg-red-600 py-9 flex px-11  justify-evenly  gap-y-6 gap-24 flex-wrap text-white">
+      <div className="icon flex justify-between mr-20 items-center ">
+        <img className="w-8 mr-3" src={call} />
+        <h3>Toll free 1800 200 1234</h3>
       </div>
       <div className="icon flex justify-center mr-20 items-center">
-      <img className="w-9 mr-3" src={world} />
-      <h3>www.crigroups.com</h3>
+        <img className="w-9 mr-3" src={facebook} />
+        <h3>www.facebook.com/cripumps</h3>
       </div>
-  
+      <div className="icon flex justify-center mr-20 items-center">
+        <img className="w-9 mr-3" src={world} />
+        <h3>www.crigroups.com</h3>
+      </div>
     </div>
   );
 };
